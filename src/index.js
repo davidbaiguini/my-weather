a1bbde19990f81d7325ed7364a7639a9edcd1bc1
David Baiguini <david.baiguini@accedo.tv>
2018-03-12 10:27:06 +1300

    remove un-used index.css

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import thunk from 'redux-thunk';
 
 import App from 'components/App';
 
-import './index.css';
-
 // Add the reducer to your store on the `routing` key
 const store = createStore(
     combineReducers(
